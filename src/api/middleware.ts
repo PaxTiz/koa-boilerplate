ca3c359e8e18bb4025e97cb0d303cdc15d4a47b2
Valentin Cernuta <vcernuta3@gmail.com>
2023-01-22 17:28:27 +0100

    feat: add validation for files

--- src/api/middleware.ts
+++ src/api/middleware.ts
@@ -31,10 +31,21 @@ export const isAuthenticated = async (
   return next();
 };
 
+type FileValidator = {
+  /** Maximum size of file in bytes */
+  size?: number;
+
+  /** List of allowed mime types */
+  mimeTypes?: string | Array<string> | ((mimeType: string) => string | null);
+
+  /** Mark the file as required, send error if not provided */
+  required?: boolean;
+};
 type ValidateSchema = {
   body?: Schema;
   params?: Schema;
   query?: Schema;
+  files?: Record<string, FileValidator>;
 };
 
 const formatErrors = (errors: Record<string, any>) => {
@@ -74,6 +85,63 @@ export const validate = (schemas: ValidateSchema) => {
         ...allErrors,
         ...validateSchema(schemas.query, context.request.query),
       ];
+    }
+
+    if (schemas.files) {
+      const files = context.request.files;
+      for (const [fileKey, validator] of Object.entries(schemas.files)) {
+        if (validator.required && (!files || !files[fileKey])) {
+          allErrors = [...allErrors, new FormError(fileKey, "file_required")];
+          continue;
+        }
+
+        if (files && files[fileKey]) {
+          const file = files[fileKey];
+          if (Array.isArray(file)) {
+            allErrors = [
+              ...allErrors,
+              new FormError(fileKey, "file_cant_be_array"),
+            ];
+            continue;
+          }
+
+          if (validator.size && file.size > validator.size) {
+            allErrors = [...allErrors, new FormError(fileKey, "file_too_big")];
+            continue;
+          }
+
+          if (validator.mimeTypes) {
+            if (!file.mimetype) {
+              allErrors = [
+                ...allErrors,
+                new FormError(fileKey, "invalid_file"),
+              ];
+              continue;
+            }
+
+            if (typeof validator.mimeTypes === "function") {
+              const response = validator.mimeTypes(file.mimetype);
+              if (response) {
+                allErrors = [...allErrors, new FormError(fileKey, response)];
+              }
+
+              continue;
+            }
+
+            const mimeTypes = Array.isArray(validator.mimeTypes)
+              ? validator.mimeTypes
+              : [validator.mimeTypes];
+
+            if (!mimeTypes.includes(file.mimetype)) {
+              allErrors = [
+                ...allErrors,
+                new FormError(fileKey, "file_type_invalid"),
+              ];
+              continue;
+            }
+          }
+        }
+      }
     }
 
     if (allErrors.length) {
