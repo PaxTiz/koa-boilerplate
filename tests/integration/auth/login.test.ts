89384ed5031673e52011b4974e7892e9f879afb3
Valentin Cernuta <vcernuta3@gmail.com>
2023-06-09 22:03:03 +0200

    feat: install vitest

---
+++ tests/integration/auth/login.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from "vitest";
+
+describe("Authentication", () => {
+  describe("Login", () => {
+    it("should login", () => {
+      expect(true).toBe(true);
+    });
+  });
+});
